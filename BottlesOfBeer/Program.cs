c3631155505ab47fbd2374e9644de323765ce093
msunkist99 <msunkist99@gmail.com>
2019-01-26 18:59:36 -0600

    1/26/2019 add for loop to print 99 bottles of beer

--- BottlesOfBeer/Program.cs
+++ BottlesOfBeer/Program.cs
@@ -9,6 +9,10 @@ namespace BottlesOfBeer
             // TODO: Output "99 bottles of beer on the wall.  99 bottles of beer.  Take 1 down and pass it around, 98 bottles of beer on the wall.";
             // Count down until there are no more bottles of beer on the wall.
 
+            for (int i = 99; i > 0; i--)
+            {
+                Console.WriteLine("{0} bottles of beer on the wall.  {0} bottles of beer.   Take 1 down and pass it around, {1} bottles of beer on the wall.", i, (i-1));
+            }
             Console.ReadLine();
         }
     }
